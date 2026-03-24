798bb599a3ea1f85d58a98e99ad4a0de70bf6024
malekalshaabii <malekalshaabi99@gmail.com>
2026-03-24 17:38:28 +0300

    Delete TaskListt directory

--- TaskListt/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include('config.php');
-$user_id = $_SESSION['user_id'];
-$ID=$_GET['id'];
-mysqli_query($con ,"DELETE FROM tasks where id=$ID AND user_id=$user_id ");
-header('location:task.php');
-?>
